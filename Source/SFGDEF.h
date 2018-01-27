e5682edb384910bc81216d257ba03f60dd086ccc
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-01-27 20:58:12 +0500

    [lib] Added GDEF table macros...

--- Source/SFGDEF.h
+++ Source/SFGDEF.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Muhammad Tayyab Akram
+ * Copyright (C) 2018 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,9 +34,19 @@ typedef SFUInt16 SFGlyphClassValue;
 #define SFGDEF_Version(data)                            SFData_UInt32(data, 0)
 #define SFGDEF_GlyphClassDefOffset(data)                SFData_UInt16(data, 4)
 #define SFGDEF_AttachListOffset(data)                   SFData_UInt16(data, 6)
-#define SFGDEF_LigCarretListOffset(data)                SFData_UInt16(data, 8)
+#define SFGDEF_LigCaretListOffset(data)                 SFData_UInt16(data, 8)
 #define SFGDEF_MarkAttachClassDefOffset(data)           SFData_UInt16(data, 10)
 #define SFGDEF_MarkGlyphSetsDefOffset(data)             SFData_UInt16(data, 12)
+#define SFGDEF_GlyphClassDefTable(data) \
+    SFData_Subdata(data, SFGDEF_GlyphClassDefOffset(data))
+#define SFGDEF_AttachListTable(data) \
+    SFData_Subdata(data, SFGDEF_AttachListOffset(data))
+#define SFGDEF_LigCaretListTable(data) \
+    SFData_Subdata(data, SFGDEF_LigCaretListOffset(data))
+#define SFGDEF_MarkAttachClassDefTable(data) \
+    SFData_Subdata(data, SFGDEF_MarkAttachClassDefOffset(data))
+#define SFGDEF_MarkGlyphSetsDefTable(data) \
+    SFData_Subdata(data, SFGDEF_MarkGlyphSetsDefOffset(data))
 
 /**************************************************************************************************/
 
@@ -44,7 +54,11 @@ typedef SFUInt16 SFGlyphClassValue;
 
 #define SFAttachList_CoverageOffset(data)               SFData_UInt16(data, 0)
 #define SFAttachList_GlyphCount(data)                   SFData_UInt16(data, 2)
-#define SFAttachList_AttachPointOffset(data)            SFData_UInt16(data, 4)
+#define SFAttachList_AttachPointOffset(data, index)     SFData_UInt16(data, 4 + ((index) * 2))
+#define SFAttachList_CoverageTable(data) \
+    SFData_Subdata(data, SFAttachList_CoverageOffset(data))
+#define SFAttachList_AttachPointTable(data, index) \
+    SFData_Subdata(data, SFAttachList_AttachPointOffset(data, index))
 
 #define SFAttachPoint_PointCount(data)                  SFData_UInt16(data, 0)
 #define SFAttachPoint_PointIndex(data, index)           SFData_UInt16(data, 2 + ((index) * 2))
@@ -56,9 +70,15 @@ typedef SFUInt16 SFGlyphClassValue;
 #define SFLigCarretList_CoverageOffset(data)            SFData_UInt16(data, 0)
 #define SFLigCarretList_LigGlyphCount(data)             SFData_UInt16(data, 2)
 #define SFLigCarretList_LigGlyphOffset(data, index)     SFData_UInt16(data, 4 + ((index) * 2))
+#define SFLigCarretList_CoverageTable(data) \
+    SFData_Subdata(data, SFLigCarretList_CoverageOffset(data))
+#define SFLigCarretList_LigGlyphIndex(data, index) \
+    SFData_Subdata(data, SFLigCarretList_LigGlyphOffset(data, index))
 
 #define SFLigGlyph_CaretCount(data)                     SFData_UInt16(data, 0)
 #define SFLigGlyph_CaretValueOffset(data, index)        SFData_UInt16(data, 2 + ((index) * 2))
+#define SFLigGlyph_CaretValueTable(data, index) \
+    SFData_Subdata(data, SFLigGlyph_CaretValueOffset(data, index))
 
 #define SFCaretValue_Format(data)                       SFData_UInt16(data, 0)
 
@@ -66,8 +86,10 @@ typedef SFUInt16 SFGlyphClassValue;
 
 #define SFCaretValueF2_CaretValuePoint(data)            SFData_UInt16(data, 2)
 
-#define SFCaretValueF3_Coordinate(data)                 SFData_Int16(data, 2)
-#define SFCaretValueF3_DeviceTableOffset(data)          SFData_UInt16(data, 4)
+#define SFCaretValueF3_Coordinate(data)                 SFData_Int16 (data, 2)
+#define SFCaretValueF3_DeviceOffset(data)               SFData_UInt16(data, 4)
+#define SFCaretValueF3_DeviceTable(data) \
+    SFData_Subdata(data, SFCaretValueF3_DeviceOffset(data))
 
 /**************************************************************************************************/
 
@@ -76,6 +98,8 @@ typedef SFUInt16 SFGlyphClassValue;
 #define SFMarkGlyphSets_Format(data)                    SFData_UInt16(data, 0)
 #define SFMarkGlyphSets_MarkSetCount(data)              SFData_UInt16(data, 2)
 #define SFMarkGlyphSets_CoverageOffset(data, index)     SFData_UInt32(data, 4 + ((index) * 4))
+#define SFMarkGlyphSets_CoverageTable(data, index) \
+    SFData_Subdata(data, SFMarkGlyphSets_CoverageOffset(data, index))
 
 /**************************************************************************************************/
 
