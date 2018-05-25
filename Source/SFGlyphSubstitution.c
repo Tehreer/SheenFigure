50d696942d3b5ffa7085ea0cec63149a0bc7ead9
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-05-25 20:28:17 +0500

    [lib] Enabled reverse chain substitution

--- Source/SFGlyphSubstitution.c
+++ Source/SFGlyphSubstitution.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Muhammad Tayyab Akram
+ * Copyright (C) 2015-2018 Muhammad Tayyab Akram
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ SF_PRIVATE SFBoolean _SFApplySubstitutionSubtable(SFTextProcessorRef textProcess
             return _SFApplyExtensionSubtable(textProcessor, subtable);
 
         case SFLookupTypeReverseChainingContext:
-            break;
+            return _SFApplyReverseChainSubst(textProcessor, subtable);
     }
 
     return SFFalse;
