d6a18dc810dde4d8300f5602a5965da3e9b654b3
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2018-09-28 23:46:55 +0500

    [test] Used tag function to create SFNT tags in scheme tester

--- Tools/Tester/SchemeTester.cpp
+++ Tools/Tester/SchemeTester.cpp
@@ -28,12 +28,14 @@ extern "C" {
 #include "OpenType/Builder.h"
 #include "OpenType/Common.h"
 #include "OpenType/GSUB.h"
+#include "Utilities/General.h"
 #include "Utilities/SFPattern+Testing.h"
 #include "SchemeTester.h"
 
 using namespace std;
 using namespace SheenFigure::Tester;
 using namespace SheenFigure::Tester::OpenType;
+using namespace SheenFigure::Tester::Utilities;
 
 class TestKnowledge {
 public:
@@ -53,7 +55,7 @@ private:
 
     static SFScriptKnowledgeRef seekScript(const void *, SFTag scriptTag)
     {
-        if (scriptTag == 'test') {
+        if (scriptTag == tag("test")) {
             return &object->knowledge.script;
         }
 
@@ -63,20 +65,20 @@ private:
     TestKnowledge() {
         features.subst = {
             /* First Group. */
-            { 1, 'srqi', REQUIRED,       INDIVIDUAL,   0x00 },
-            { 1, 'srq1', REQUIRED,       SIMULTANEOUS, 0x00 },
-            { 1, 'son1', ON_BY_DEFAULT,  SIMULTANEOUS, 0x00 },
-            { 1, 'sof1', OFF_BY_DEFAULT, SIMULTANEOUS, 0x00 },
+            { 1, tag("srqi"), REQUIRED,       INDIVIDUAL,   0x00 },
+            { 1, tag("srq1"), REQUIRED,       SIMULTANEOUS, 0x00 },
+            { 1, tag("son1"), ON_BY_DEFAULT,  SIMULTANEOUS, 0x00 },
+            { 1, tag("sof1"), OFF_BY_DEFAULT, SIMULTANEOUS, 0x00 },
             /* Second Group. */
-            { 2, 'sim1', REQUIRED,       SIMULTANEOUS, 0x00 },
-            { 2, 'sim2', REQUIRED,       SIMULTANEOUS, 0x00 },
+            { 2, tag("sim1"), REQUIRED,       SIMULTANEOUS, 0x00 },
+            { 2, tag("sim2"), REQUIRED,       SIMULTANEOUS, 0x00 },
         };
         features.pos = {
             /* Third Group. */
-            { 3, 'pof1', OFF_BY_DEFAULT, SIMULTANEOUS, 0x00 },
-            { 3, 'pon1', ON_BY_DEFAULT,  SIMULTANEOUS, 0x00 },
-            { 3, 'prq1', REQUIRED,       SIMULTANEOUS, 0x00 },
-            { 3, 'prqi', REQUIRED,       INDIVIDUAL,   0x00 },
+            { 3, tag("pof1"), OFF_BY_DEFAULT, SIMULTANEOUS, 0x00 },
+            { 3, tag("pon1"), ON_BY_DEFAULT,  SIMULTANEOUS, 0x00 },
+            { 3, tag("prq1"), REQUIRED,       SIMULTANEOUS, 0x00 },
+            { 3, tag("prqi"), REQUIRED,       INDIVIDUAL,   0x00 },
         };
         knowledge.script = {
             SFTextDirectionLeftToRight,
@@ -104,25 +106,25 @@ static const UInt16 SON1 = 10;
 static const UInt16 SRQ1 = 11;
 static const UInt16 SRQI = 12;
 
-static void loadTable(void *, SFTag tag, SFUInt8 *buffer, SFUInteger *length)
+static void loadTable(void *, SFTag tableTag, SFUInt8 *buffer, SFUInteger *length)
 {
-    if (tag == 'GSUB') {
+    if (tableTag == tag("GSUB")) {
         Builder builder;
 
         FeatureListTable &featureList = builder.createFeatureList({
-            {'cust', builder.createFeature({ CUST })},
-            {'scst', builder.createFeature({ SCST })},
-            {'sim1', builder.createFeature({ SIM1 })},
-            {'sim2', builder.createFeature({ SIM2 })},
-            {'sof1', builder.createFeature({ SOF1 })},
-            {'son1', builder.createFeature({ SON1 })},
-            {'srq1', builder.createFeature({ SRQ1 })},
-            {'srqi', builder.createFeature({ SRQI })},
+            {tag("cust"), builder.createFeature({ CUST })},
+            {tag("scst"), builder.createFeature({ SCST })},
+            {tag("sim1"), builder.createFeature({ SIM1 })},
+            {tag("sim2"), builder.createFeature({ SIM2 })},
+            {tag("sof1"), builder.createFeature({ SOF1 })},
+            {tag("son1"), builder.createFeature({ SON1 })},
+            {tag("srq1"), builder.createFeature({ SRQ1 })},
+            {tag("srqi"), builder.createFeature({ SRQI })},
         });
         ScriptListTable &scriptList = builder.createScriptList({
-            {'test', builder.createScript(builder.createLangSys({ 0, 1, 2, 3, 4, 5, 6, 7 }), {
-                                              {'LNG ', builder.createLangSys({ 7 })}
-                                          })}
+            {tag("test"), builder.createScript(builder.createLangSys({ 0, 1, 2, 3, 4, 5, 6, 7 }), {
+                                                   {tag("LNG "), builder.createLangSys({ 7 })}
+                                               })}
         });
 
         GSUB gsub;
@@ -140,20 +142,20 @@ static void loadTable(void *, SFTag tag, SFUInt8 *buffer, SFUInteger *length)
         if (buffer) {
             memcpy(buffer, writer.data(), (size_t)writer.size());
         }
-    } else if (tag == 'GPOS') {
+    } else if (tableTag == tag("GPOS")) {
         Builder builder;
 
         FeatureListTable &featureList = builder.createFeatureList({
-            {'cust', builder.createFeature({ CUST })},
-            {'pcst', builder.createFeature({ PCST })},
-            {'pof1', builder.createFeature({ POF1 })},
-            {'pon1', builder.createFeature({ PON1 })},
-            {'prq1', builder.createFeature({ PRQ1 })},
-            {'prqi', builder.createFeature({ PRQI })},
+            {tag("cust"), builder.createFeature({ CUST })},
+            {tag("pcst"), builder.createFeature({ PCST })},
+            {tag("pof1"), builder.createFeature({ POF1 })},
+            {tag("pon1"), builder.createFeature({ PON1 })},
+            {tag("prq1"), builder.createFeature({ PRQ1 })},
+            {tag("prqi"), builder.createFeature({ PRQI })},
         });
         ScriptListTable &scriptList = builder.createScriptList({
-            {'test', builder.createScript(builder.createLangSys({ 0, 1, 2, 3, 4, 5 }), {
-                                              {'LNG ', builder.createLangSys({ 2 })}
+            {tag("test"), builder.createScript(builder.createLangSys({ 0, 1, 2, 3, 4, 5 }), {
+                                              {tag("LNG "), builder.createLangSys({ 2 })}
                                           })}
         });
 
@@ -196,7 +198,7 @@ void SchemeTester::testFeatures()
         SFSchemeRef scheme = SFSchemeCreate();
 
         SFTag featureTags[] = {
-            'ccmp', 'liga', 'dist', 'kern'
+            tag("ccmp"), tag("liga"), tag("dist"), tag("kern")
         };
         SFUInt16 featureValues[] = {
             1, 2, 3, 4
@@ -217,7 +219,7 @@ void SchemeTester::testFeatures()
         SFSchemeRef scheme = SFSchemeCreate();
 
         SFTag featureTags[] = {
-            'ccmp', 'liga', 'dist', 'kern', 'ccmp', 'kern'
+            tag("ccmp"), tag("liga"), tag("dist"), tag("kern"), tag("ccmp"), tag("kern")
         };
         SFUInt16 featureValues[] = {
             1, 2, 3, 4, 5, 6
@@ -227,7 +229,7 @@ void SchemeTester::testFeatures()
         SFSchemeSetFeatureValues(scheme, featureTags, featureValues, featureCount);
 
         SFTag expectedTags[] = {
-            'ccmp', 'liga', 'dist', 'kern'
+            tag("ccmp"), tag("liga"), tag("dist"), tag("kern")
         };
         SFUInt16 expectedValues[] = {
             5, 2, 3, 6
@@ -255,16 +257,16 @@ void SchemeTester::testBuild()
     SFScheme scheme;
     SFSchemeInitialize(&scheme, TestKnowledge::instance());
     SFSchemeSetFont(&scheme, font);
-    SFSchemeSetScriptTag(&scheme, 'test');
+    SFSchemeSetScriptTag(&scheme, tag("test"));
 
     /* Test with default language. */
     {
-        SFSchemeSetLanguageTag(&scheme, 'dflt');
+        SFSchemeSetLanguageTag(&scheme, tag("dflt"));
         SFPatternRef pattern = SFSchemeBuildPattern(&scheme);
 
         SFTag expectedTags[] = {
-            'srqi', 'srq1', 'son1', 'sim1', 'sim2',
-            'pon1', 'prq1', 'prqi'
+            tag("srqi"), tag("srq1"), tag("son1"), tag("sim1"), tag("sim2"),
+            tag("pon1"), tag("prq1"), tag("prqi")
         };
         SFLookupInfo expectedLookups[] = {
             {SRQI, 1}, {SON1, 1}, {SRQ1, 1}, {SIM1, 1}, {SIM2, 1},
@@ -281,8 +283,8 @@ void SchemeTester::testBuild()
             .font = font,
             .featureTags = { expectedTags, 8 },
             .featureUnits = { expectedUnits, 3, 2 },
-            .scriptTag = 'test',
-            .languageTag = 'dflt',
+            .scriptTag = tag("test"),
+            .languageTag = tag("dflt"),
             .defaultDirection = SFTextDirectionLeftToRight,
         };
         assert(SFPatternEqualToPattern(pattern, &expectedPattern));
@@ -292,10 +294,10 @@ void SchemeTester::testBuild()
 
     /* Test with a non-default language. */
     {
-        SFSchemeSetLanguageTag(&scheme, 'LNG ');
+        SFSchemeSetLanguageTag(&scheme, tag("LNG "));
         SFPatternRef pattern = SFSchemeBuildPattern(&scheme);
 
-        SFTag expectedTags[] = { 'srqi' };
+        SFTag expectedTags[] = { tag("srqi") };
         SFLookupInfo expectedLookups[] = { {SRQI, 1} };
         SFFeatureUnit expectedUnits[] = {
             { { &expectedLookups[0], 1 }, { 0, 1 }, 0x00 },
@@ -304,8 +306,8 @@ void SchemeTester::testBuild()
             .font = font,
             .featureTags = { expectedTags, 1 },
             .featureUnits = { expectedUnits, 1, 0 },
-            .scriptTag = 'test',
-            .languageTag = 'LNG ',
+            .scriptTag = tag("test"),
+            .languageTag = tag("LNG "),
             .defaultDirection = SFTextDirectionLeftToRight,
         };
         assert(SFPatternEqualToPattern(pattern, &expectedPattern));
@@ -324,20 +326,21 @@ void SchemeTester::testBuild()
     /* Test by enabling a custom feature available in both GSUB and GPOS. */
     {
         SFTag featureTags[] = {
-            'srqi', 'prq1', 'son1', 'sof1', 'prqi', 'pon1', 'pof1', 'scst', 'pcst', 'cust'
+            tag("srqi"), tag("prq1"), tag("son1"), tag("sof1"), tag("prqi"),
+            tag("pon1"), tag("pof1"), tag("scst"), tag("pcst"), tag("cust")
         };
         SFUInt16 featureValues[] = {
             0, 0, 0, 1, 2, 3, 4, 5, 6, 7
         };
 
-        SFSchemeSetLanguageTag(&scheme, 'dflt');
+        SFSchemeSetLanguageTag(&scheme, tag("dflt"));
         SFSchemeSetFeatureValues(&scheme, featureTags, featureValues, sizeof(featureValues) / sizeof(SFUInt16));
 
         SFPatternRef pattern = SFSchemeBuildPattern(&scheme);
 
         SFTag expectedTags[] = {
-            'srqi', 'srq1', 'sof1', 'sim1', 'sim2', 'scst', 'cust',
-            'pof1', 'pon1', 'prq1', 'prqi', 'pcst'
+            tag("srqi"), tag("srq1"), tag("sof1"), tag("sim1"), tag("sim2"), tag("scst"), tag("cust"),
+            tag("pof1"), tag("pon1"), tag("prq1"), tag("prqi"), tag("pcst")
         };
         SFLookupInfo expectedLookups[] = {
             {SRQI, 1}, {SOF1, 1}, {SRQ1, 1}, {SIM1, 1}, {SIM2, 1}, {CUST, 7}, {SCST, 5},
@@ -356,8 +359,8 @@ void SchemeTester::testBuild()
             .font = font,
             .featureTags = { expectedTags, 12 },
             .featureUnits = { expectedUnits, 4, 3 },
-            .scriptTag = 'test',
-            .languageTag = 'dflt',
+            .scriptTag = tag("test"),
+            .languageTag = tag("dflt"),
             .defaultDirection = SFTextDirectionLeftToRight,
         };
         assert(SFPatternEqualToPattern(pattern, &expectedPattern));
